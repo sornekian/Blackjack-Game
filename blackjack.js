f2a08d9be8404fdcddc23e19ebf432412f56e731
sornekian <sornekian_sarvornek@gmail.com>
2023-01-04 09:59:23 -0800

    commit5

--- blackjack.js
+++ blackjack.js
@@ -2,7 +2,7 @@
     const faces = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K'];
     const suits = ['s', 'h', 'c', 'd'];
     let deck = [];
-
+// creating the deck
     function generateDeck() {
         suits.forEach(suit => {
             faces.forEach(face => {
@@ -12,7 +12,7 @@
             })
         })
     }
-
+// shuffling the deck
 function shuffleDeck() {
     for (var i = 0; i < 52; i++) {
         var newCard = deck[i];
@@ -22,7 +22,7 @@ function shuffleDeck() {
 }}
 generateDeck()
 shuffleDeck()
-
+// passing out the cards
 function renderDeck() {
       const cardEl = document.createElement('div')
       cardEl.className = 'card ' + deck[0].face
@@ -39,11 +39,29 @@ function renderDeck() {
       cardEl4 = document.createElement('div')
       cardEl4.className = 'card ' + deck[3].face
       document.querySelector("#dealer").append(cardEl4)
+}
+renderDeck()
+// making the hit button add one card each time
 
+function hitPlayer() {
+    let cardEl5 = document.createElement('div');
+    let cardEl5.className = deck.pop();
 
 }
 
-renderDeck()
+hit.addEventListener('click', hitPlayer);
+
+// starting the game
+function startGame() {
+
+}
+
+// making stay pass to dealer
+function stay() {
+    
+}
+
+
 // hit.addEventListener('click', hitPlayer);
 // stay.addEventListener('click', hitDealer);
 // start.addEventListener('click', resetGame)
