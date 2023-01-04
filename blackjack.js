f12761b7a888a0050301ec12c152d1a7eb3c4808
sornekian <sornekian_sarvornek@gmail.com>
2023-01-04 11:43:05 -0800

    commit6

--- blackjack.js
+++ blackjack.js
@@ -3,29 +3,52 @@
     const suits = ['s', 'h', 'c', 'd'];
     let deck = [];
 // creating the deck
-    function generateDeck() {
-        suits.forEach(suit => {
-            faces.forEach(face => {
-                 deck.push({
-                    'face': suit + face,
-                })
-            })
-        })
+function generateDeck() {
+    console.log("generating deck")
+    suits.forEach(suit => {
+        faces.forEach(face => {
+             deck.push({
+                 'face': `card ${suit}${face}`,
+             })
+                // console.log(deck[deck.length - 1])
+         })
+     })
+ }
+    const cardValue = {
+        'J': 10,
+        'Q': 10,
+        'K': 10,
+        'A': 1 || 11,
+        '02': 2,
+        '03': 3,
+        '04': 4, 
+        '05': 5,
+        '06': 6,
+        '07': 7,
+        '08': 8,
+        '09': 9,
+        '10': 10
+
     }
-// shuffling the deck
-function shuffleDeck() {
-    for (var i = 0; i < 52; i++) {
-        var newCard = deck[i];
-        var randomCard = Math.floor(Math.random() * 52);
-        deck[i] = deck[randomCard];
-        deck[randomCard] = newCard;
-}}
+
+
+// passing out the deck
+function dealCard() {
+    // for (let i = 0; i < 52; i++) {
+    const randomCard = Math.floor(Math.random() * (deck.length - 1));
+    console.log(randomCard)
+    const newCard = deck[randomCard];
+    deck.splice(randomCard, 1);
+    if(deck.length === 0) generateDeck();
+    return newCard;
+} 
+// }} 
 generateDeck()
-shuffleDeck()
-// passing out the cards
+dealCard()
+// passing out the cards to each player
 function renderDeck() {
       const cardEl = document.createElement('div')
-      cardEl.className = 'card ' + deck[0].face
+      cardEl.className = dealCard().face
       document.querySelector("#player").append(cardEl)
 
       cardEl2 = document.createElement('div')
@@ -33,23 +56,31 @@ function renderDeck() {
       document.querySelector("#dealer").append(cardEl2)
 
       const cardEl3 = document.createElement('div')
-      cardEl3.className = 'card ' + deck[2].face
+      cardEl3.className = dealCard().face
       document.querySelector("#player").append(cardEl3)
 
       cardEl4 = document.createElement('div')
-      cardEl4.className = 'card ' + deck[3].face
+      cardEl4.className = dealCard().face
       document.querySelector("#dealer").append(cardEl4)
+
 }
 renderDeck()
 // making the hit button add one card each time
 
 function hitPlayer() {
-    let cardEl5 = document.createElement('div');
-    let cardEl5.className = deck.pop();
+    const cardEl5 = document.createElement('div')
+    cardEl5.className = dealCard().face
+    document.querySelector("#player").append(cardEl5)
 
+}
+function stayPlayer() {
+    const cardEl6 = document.createElement('div')
+    cardEl6.className = dealCard().face
+    document.querySelector("#dealer").append(cardEl6)
 }
 
 hit.addEventListener('click', hitPlayer);
+hit.addEventListener('click', stayPlayer);
 
 // starting the game
 function startGame() {
@@ -58,7 +89,7 @@ function startGame() {
 
 // making stay pass to dealer
 function stay() {
-    
+
 }
 
 
