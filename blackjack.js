eb5c9b7f920fefbaae1f3b5e3bf118b05e801898
sornekian <sornekian_sarvornek@gmail.com>
2023-01-04 18:37:48 -0800

    commit10

--- blackjack.js
+++ blackjack.js
@@ -3,32 +3,13 @@
     const suits = ['s', 'h', 'c', 'd'];
     let deck = [];
 
-    // let cardValue = {
-    //     '10': 10,
-    //     'J': 10,
-    //     'Q': 10,
-    //     'K': 10,  
-    //     'A': 11,
-    //     '02': 2,
-    //     '03': 3,
-    //     '04': 4, 
-    //     '05': 5,
-    //     '06': 6,
-    //     '07': 7,
-    //     '08': 8,
-    //     '09': 9 
-    // }
-// creating the deck
 function generateDeck() {
     console.log("generating deck")
     suits.forEach(suit => {
         faces.forEach(face => {
              deck.push({
-                 'face': `card ${suit}${face}`,
-                //  'cardVal': cardValue[face]
-                 
+                 'face': `card ${suit}${face}`,                             
              })
-                // console.log(deck[deck.length - 1])
          })
      })     
  }
@@ -37,16 +18,12 @@ let playerHand = []
 let dealerHand = []
 
 function dealCard() {
-    // for (let i = 0; i < 52; i++) {
     const randomCard = Math.floor(Math.random() * (deck.length - 1));
-    // console.log(randomCard)
     const newCard = deck[randomCard];
-    // playerHand.push(deck[randomCard]);
     deck.splice(randomCard, 1);
     if(deck.length === 0) generateDeck();
     return newCard;
 } 
-// }} 
 generateDeck()
 dealCard()
 
@@ -55,7 +32,6 @@ function renderDeck() {
       cardEl.className = dealCard().face
       document.querySelector("#player").append(cardEl)
       
-
       const cardEl2 = document.createElement('div')
       cardEl2.className = 'card back'
       document.querySelector("#dealer").append(cardEl2)
@@ -70,6 +46,8 @@ function renderDeck() {
 
       playerHand.push(cardEl.className)
       playerHand.push(cardEl3.className)
+    //   dealerHand.push(cardEl4.className)
+    //   dealerHand.push(cardEl4.className)
 
 }
 renderDeck()
@@ -81,30 +59,29 @@ function hitPlayer() {
     cardEl5.className = dealCard().face
     document.querySelector("#player").append(cardEl5)
     playerHand.push(cardEl5.className)
-    if (counter === 0) {
-        for (i = 0; i < playerHand.length; i++) {
-        playerHand[i] = playerHand[i].substring(6, playerHand[i].length)
+        if (counter === 0) {
+            for (i = 0; i < playerHand.length; i++) {
+            playerHand[i] = playerHand[i].substring(6, playerHand[i].length)
+            }
         }
-    }
-    if (counter > 0) {
-        playerHand[playerHand.length - 1] = playerHand[playerHand.length - 1].substring(6, playerHand[playerHand.length - 1].length)
-    }
-
-    for (i = 0; i < playerHand.length; i++) {
-       if (playerHand[i] === "A") {
-        playerHand[i] = 11
-       } 
-       if (playerHand[i] === "K") {
-        playerHand[i] = 10
-       } 
-       if (playerHand[i] === "Q") {
-        playerHand[i] = 10
-       } 
-       if (playerHand[i] === "J") {
-        playerHand[i] = 10
-       } 
-       playerHand[i] = parseInt(playerHand[i], 10)
-    } 
+        if (counter > 0) {
+            playerHand[playerHand.length - 1] = playerHand[playerHand.length - 1].substring(6, playerHand[playerHand.length - 1].length)
+        }
+        for (i = 0; i < playerHand.length; i++) {
+            if (playerHand[i] === "A") {
+            playerHand[i] = 11
+            } 
+            if (playerHand[i] === "K") {
+            playerHand[i] = 10
+            } 
+            if (playerHand[i] === "Q") {
+            playerHand[i] = 10
+            } 
+            if (playerHand[i] === "J") {
+            playerHand[i] = 10
+            } 
+            playerHand[i] = parseInt(playerHand[i], 10)
+        } 
     let sum = 0
     playerHand.forEach(function(cardValue) {
         sum += cardValue
@@ -113,36 +90,34 @@ function hitPlayer() {
     counter++
     console.log(playerHand)
 } 
-
 function hitDealer() {
     const cardEl6 = document.createElement('div')
     cardEl6.className = dealCard().face
     document.querySelector("#dealer").append(cardEl6)
     dealerHand.push(cardEl6.className)
-    if (counter === 0) {
-        for (i = 0; i < dealerHand.length; i++) {
-            dealerHand[i] = dealerHand[i].substring(6, dealerHand[i].length)
+        if (counter === 0) {
+            for (i = 0; i < dealerHand.length; i++) {
+                dealerHand[i] = dealerHand[i].substring(6, dealerHand[i].length)
+            }
+        } 
+        if (counter > 0) {
+            dealerHand[dealerHand.length - 1] = dealerHand[dealerHand.length - 1].substring(6, dealerHand[dealerHand.length - 1].length)
         }
-    }
-    if (counter > 0) {
-        dealerHand[dealerHand.length - 1] = dealerHand[dealerHand.length - 1].substring(6, dealerHand[dealerHand.length - 1].length)
-    }
-
-    for (i = 0; i < dealerHand.length; i++) {
-       if (dealerHand[i] === "A") {
-        dealerHand[i] = 11
-       } 
-       if (dealerHand[i] === "K") {
-        dealerHand[i] = 10
-       } 
-       if (dealerHand[i] === "Q") {
-        dealerHand[i] = 10
-       } 
-       if (dealerHand[i] === "J") {
-        dealerHand[i] = 10
-       } 
-       dealerHand[i] = parseInt(dealerHand[i], 10)
-    } 
+        for (i = 0; i < dealerHand.length; i++) {
+            if (dealerHand[i] === "A") {
+            dealerHand[i] = 11
+            } 
+            if (dealerHand[i] === "K") {
+            dealerHand[i] = 10
+            } 
+            if (dealerHand[i] === "Q") {
+            dealerHand[i] = 10
+            } 
+            if (dealerHand[i] === "J") {
+            dealerHand[i] = 10
+            } 
+            dealerHand[i] = parseInt(dealerHand[i], 10)
+        } 
     let sum = 0
     dealerHand.forEach(function(cardValue) {
         sum += cardValue
@@ -152,16 +127,14 @@ function hitDealer() {
     console.log(dealerHand)
 }
 
+
+// function resetGame(renderDeck())
+
+
+
 hit.addEventListener('click', hitPlayer);
 stay.addEventListener('click', hitDealer);
+start.addEventListener('click', resetGame);
 
-
-
-// starting the game
-// function startGame() {
-
-// }
-
-// start.addEventListener('click', resetGame)
 
 console.log(deck)
